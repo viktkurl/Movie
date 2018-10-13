2e8204b2bd8be16be65cb197349ee3aac4b07476
Davit Karapetian <karapetiandav@gmail.com>
2018-10-13 15:53:29 +0300

    detail movie mvp contract

---
+++ app/src/main/java/ru/vd/movie/ui/MovieDetailContract.kt
@@ -0,0 +1,21 @@
+package ru.vd.movie.ui
+
+import ru.vd.movie.BasePresenter
+import ru.vd.movie.BaseView
+import ru.vd.movie.models.DetailedMovie
+
+
+interface MovieDetailContract {
+
+    interface View: BaseView<Presenter> {
+        fun setLoadingIndicator(isLoading: Boolean)
+
+        fun showMovie(detailedMovie: DetailedMovie)
+
+        fun showMovieError()
+    }
+
+    interface Presenter: BasePresenter<View> {
+        fun loadMovie()
+    }
+}
